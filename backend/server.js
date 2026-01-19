f1f9a888508eaabc74d9a87e1d77ac42e8015b5c
SEMED-PATRIMONIO <gatsuporte2@gmail.com>
2026-01-18 23:22:44 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 const cors = require('cors');
 const db = require('./db');
 const { verificarToken } = require('./auth/auth.middleware');

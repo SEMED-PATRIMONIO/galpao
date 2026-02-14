df1c3077ccd580fc3c179f3891fe950cdfe33d9a
SEMED-PATRIMONIO <gatsuporte2@gmail.com>
2026-02-14 00:33:13 -0300

    Add files via upload

--- frontend/assets/index.php
+++ frontend/assets/index.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once 'db.php'; 
+require_once '../db.php';; 
 
 // 1. Captura de Filtros
 $filtroEscola = $_GET['escola'] ?? 'TODAS';

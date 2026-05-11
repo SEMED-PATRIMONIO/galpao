ddd6b5a8bea5ac4e305815cfa1dfc67749e1df92
SEMED-PATRIMONIO <gatsuporte2@gmail.com>
2026-05-11 20:17:35 -0300

    Delete frontend/assets/sistema-formacao directory

--- frontend/assets/sistema-formacao/app-admin/src/main.jsx
+++
@@ -1,5 +0,0 @@
-// app-professor/src/main.jsx e app-admin/src/main.jsx (IGUAIS)
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
